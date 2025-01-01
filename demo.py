e0f0f5cb8db56bd4fff0a701e92d0e0d62b1fe3b
katravath manohar <126311668+KatravathManohar@users.noreply.github.com>
2025-01-01 20:25:35 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,10 @@
+email="ilearniexcel@gmail.com"
+print("The position of @ is",email.find("@"))
+
+print("Extracting only.com from email is",email[-4:])
+
+print("Extracting gmail from the email is ",email[-9:-4])
+
+print("splitiing email based on @",email.split("@"))
+
+
